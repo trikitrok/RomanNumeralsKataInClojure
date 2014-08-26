77f18264230e1dec0422b7e5f2ce22fa4d8e24cc
trikitrok <trikitrok@gmail.com>
2014-08-26 21:09:41 +0200

    Refactor Reducing number of cases

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -19,14 +19,10 @@
                (convert from medium to (- arabic from)))))
 
 (defn arabic-to-roman [arabic]
-  (cond 
+  (cond
     (<= arabic 10) (convert 1 5 10 arabic)
-    (= arabic 40) "XL"
-    (= arabic 50) "L"
-    (= arabic 90) "XC"
-    (= arabic 100) "C"
-    (> arabic 100) (str "C" (arabic-to-roman (- arabic 100)))
-    (> arabic 90) (str "XC" (arabic-to-roman (- arabic 90)))
-    (> arabic 50) (str "L" (arabic-to-roman (- arabic 50)))
-    (> arabic 40) (str "XL" (arabic-to-roman (- arabic 40)))
+    (>= arabic 100) (str "C" (arabic-to-roman (- arabic 100)))
+    (>= arabic 90) (str "XC" (arabic-to-roman (- arabic 90)))
+    (>= arabic 50) (str "L" (arabic-to-roman (- arabic 50)))
+    (>= arabic 40) (str "XL" (arabic-to-roman (- arabic 40)))
     :else (str "X" (arabic-to-roman (- arabic 10)))))

0db25f86f13d3f382dfb8c3cc880465d840bdb9f
trikitrok <trikitrok@gmail.com>
2014-08-26 19:11:22 +0200

    It can convert 1

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,3 +1,4 @@
 (ns roman-numerals.core)
 
-
+(defn arabic-to-roman [arabic]
+  "I")
--- test/roman_numerals/core_test.clj
+++ test/roman_numerals/core_test.clj
@@ -2,5 +2,7 @@
   (:use midje.sweet)
   (:use [roman-numerals.core]))
 
-(facts "about arabic to roman numerals conversion"
- )
+(facts 
+  "about arabic to roman numerals conversion"
+  (fact "1 converts to I"
+        (arabic-to-roman 1) => "I"))

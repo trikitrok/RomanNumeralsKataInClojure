bdebfce00ddce749759791a74c13250b60d3d1f4
trikitrok <trikitrok@gmail.com>
2014-08-26 21:51:02 +0200

    Refactor removed some duplication

--- src/roman_numerals/core.clj
+++ src/roman_numerals/core.clj
@@ -1,28 +1,24 @@
 (ns roman-numerals.core)
 
 (def ^:private roman-symbols
-  {0 ""
-   1 "I"
-   5 "V"
-   10 "X"})
+  {:until-10 "IVX"
+   :until-100 "XLC"
+   :until-1000 "CDM"})
 
-(defn- convert [from medium to arabic]
-  (cond
-    (= arabic 0) ""
-    (= arabic to) (str (roman-symbols to) (convert from medium to (- arabic to)))
-    (= arabic (- medium from)) (str (roman-symbols from) (roman-symbols medium))
-    (>= arabic (- to from)) (str (roman-symbols from) (roman-symbols to) 
-                                 (convert from medium to (- arabic (- to from))))
-    (>= arabic medium) (str (roman-symbols medium) 
-                            (convert from medium to (- arabic medium)))
-    :else (str (roman-symbols from)
-               (convert from medium to (- arabic from)))))
+(defn- convert [arabic in-range]
+  (let [[from medium to] (roman-symbols in-range)]
+    (cond
+      (= arabic 0) ""
+      (= arabic 10) (str to (convert (- arabic 10) in-range))
+      (= arabic 4) (str from medium)
+      (>= arabic 9) (str from to (convert (- arabic 9) in-range))
+      (>= arabic 5) (str medium (convert (- arabic 5) in-range))
+      :else (str from (convert (- arabic 1) in-range)))))
 
 (defn arabic-to-roman [arabic]
   (cond
-    (<= arabic 10) (convert 1 5 10 arabic)
-    (>= arabic 100) (str "C" (arabic-to-roman (- arabic 100)))
-    (>= arabic 90) (str "XC" (arabic-to-roman (- arabic 90)))
-    (>= arabic 50) (str "L" (arabic-to-roman (- arabic 50)))
-    (>= arabic 40) (str "XL" (arabic-to-roman (- arabic 40)))
-    :else (str "X" (arabic-to-roman (- arabic 10)))))
+    (<= arabic 10) (convert arabic :until-10)
+    (<= arabic 100) (str (convert (quot arabic 10) :until-100) 
+                         (arabic-to-roman (mod arabic 10)))
+    :else (str (convert (quot arabic 100) :until-1000)
+               (arabic-to-roman (mod arabic 100)))))
